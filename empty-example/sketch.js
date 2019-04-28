81cda27f5398d0394bc7e2c5f249be8ea4d199be
Guilherme Balloni <balloniguilherme7@gmail.com>
2019-04-28 15:40:45 -0300

    committ index.html

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,120 @@
+function removeFromArray(arr, elt) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] == elt) {
+      arr.splice(i, 1);
+    }
+  }
+}
+
+
+
+
+var cols = 20;
+var rows = 20;
+var grid = new Array(cols);
+
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+var w, h;
+var path = [];
+
+function Spot(i, j) {
+  this.i = i;
+  this.j = j;
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+  this.neighbors = [];
+  this.previous = undefined;
+  this.wall = false;
+
+  if (random(1) < 0.3) {
+    this.wall = true;
+  }
+
+
+  this.show = function(col) {
+    //fill(col);
+    if (this.wall) {
+      fill(0);
+      noStroke();
+      rect(this.i * w + w / 2, this.j * h + h / 2, w / 2, h / 2);
+    }
+    //rect(this.i * w, this.j * h, w - 1, h - 1);
+  }
+
+  this.addNeighbors = function(grid) {
+    var i = this.i;
+    var j = this.j;
+    if (i < cols - 1) {
+      this.neighbors.push(grid[i + 1][j]);
+    }
+    if (i > 0) {
+      this.neighbors.push(grid[i - 1][j]);
+    }
+    if (j < rows - 1) {
+      this.neighbors.push(grid[i][j + 1]);
+    }
+    if (j > 0) {
+      this.neighbors.push(grid[i][j - 1]);
+    }
+    if (i > 0 && j > 0) {
+      this.neighbors.push(grid[i - 1][j - 1]);
+    }
+    if (i < cols - 1 && j > 0) {
+      this.neighbors.push(grid[i + 1][j - 1]);
+    }
+    if (i > 0 && j < rows - 1) {
+      this.neighbors.push(grid[i - 1][j + 1]);
+    }
+    if (i < cols - 1 && j < rows - 1) {
+      this.neighbors.push(grid[i + 1][j + 1]);
+    }
+  }
+
+
+
+
+}
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+  console.log('A*');
+
+  w = width / cols;
+  h = height / rows;
+
+  // Making a 2D array
+  for (var i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+
+
+
+
 }
 
 function draw() {
-  // put drawing code here
+
+
+
+  background(400);
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show(color(200));
+    }
+  }
+
+
+
+
 }
